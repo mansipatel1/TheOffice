0056e2d674e5066c2e34df1a27e0bb07ba86e669
Mansi <mansigpatel1@gmail.com>
2018-05-17 01:20:35 -0400

    added delete button and deleted elemets from list and array

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const form = document.querySelector('#userForm')
-
+const charArray = []; 
     const renderListItem = function(label, value) {
         const item = document.createElement('li');
+        const button = document.createElement('button');
+        button.addEventListener('click', removeItem);c
+        button.textContent= 'Delete';
+        button.style.width = '50px'
+        button.style.height = '25px';
+      //  button.style. = 'center'
         item.textContent = `${label}: ${value}`;
+        item.appendChild(button);
+        
         return item;
      }
 
 
 const renderList = function(userName) {
     const list = document.createElement('ul');
-    list.appendChild(renderListItem('Name', userName));
+    list.appendChild(renderListItem('Character', userName));
     return list;
 }
 
 
+const removeItem = function(ev) {
+    ev.preventDefault();
+    del = ev.target;
+    del.parentNode.remove();
+    charArray.splice(charArray.indexOf(charArray));
+}
 
 const handleSubmit = function(ev) {
   ev.preventDefault()
@@ -22,9 +36,9 @@ const handleSubmit = function(ev) {
   const users = document.querySelector('#users')
   users.appendChild(renderList(userName));
 
-
-  f.reset()
-  f.userName.focus()
+  charArray.push(userName);
+  f.reset();
+  f.userName.focus();
 }
 
 form.addEventListener('submit', handleSubmit)
